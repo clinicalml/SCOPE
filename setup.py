a9813f789429dc3a7ec5318df4ed1b83119f15b6
zeshanmh <zeshanmh3@gmail.com>
2024-02-07 16:29:00 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from distutils.core import setup
 
 setup(name='mm79',
       version='1.0',
-      description='Modelinig multiple myeloma',
+      description='Modeling multiple myeloma',
       author='MIT',
       packages=['mm79'],
      )
